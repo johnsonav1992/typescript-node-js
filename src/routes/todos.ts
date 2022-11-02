dcef5d2643eee5abe7e827848c18b83377659d7d
Alex Johnson <johnsonav1992@gmail.com>
2022-11-02 09:32:25 -0500

    added CRUD operations

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,13 +1,12 @@
 import { Router } from 'express'
+import { createTodo, getTodos, updateTodo } from '../controllers/todos'
 
 const router = Router()
 
-router.post('/')
-
-router.get('/')
-
-router.patch('/:id')
-
+//Routes
+router.post('/', createTodo)
+router.get('/', getTodos)
+router.patch('/:id', updateTodo)
 router.delete('/:id')
 
 export default router
